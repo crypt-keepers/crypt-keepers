106d278cfa87578e8898db25ff945c32c256e12f
Claire Johnson <c.eliseee@gmail.com>
2017-10-01 12:49:07 -0700

    Added process port

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + './../public'));
+app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, function() {
   console.log(`App listening at ${port}`);
