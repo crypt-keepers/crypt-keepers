4ec78bea14f9c86a19951293a2c9b585036343c5
Claire Johnson <c.eliseee@gmail.com>
2017-10-02 14:10:37 -0700

    Modify render

--- src/components/App.js
+++ src/components/App.js
@@ -31,6 +31,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
+        Hi
         <Search/>
         <DataDisplay data={this.state.model.data}/>
         <Panel/>
