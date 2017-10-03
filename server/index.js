640f61c740f6c3a157f66a3fd90ca83a20fb8ea0
dulynoded <administrator@administrators-Mac-Pro.local>
2017-10-03 12:59:15 -0700

    (refactor) Remove string concatenation and logging

--- server/index.js
+++ server/index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const router = require('./routes');
+const path = require('path')
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
 
 
 const port = process.env.PORT || 3000;
-
-const server = app.listen(port, function() {
-  console.log(`App listening at ${port}`);
-});
+app.listen(port, () => {});
