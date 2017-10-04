424243f35c82e1555aac73a633e8ba4145bad3d9
dulynoded <administrator@administrators-Mac-Pro.local>
2017-10-03 19:05:16 -0700

    (feat) Get time series by range

--- helpers/gdax.js
+++ helpers/gdax.js
@@ -1,8 +1,6 @@
 const request = require('request');
 const rp = require('request-promise');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const getTimeSeriesByRange = (coin = 'BTC', dateStart, dateEnd, granularity, currency = 'USD') => {
   const options = {
     url: `https://api.gdax.com/products/${coin}-${currency}/candles`,
@@ -13,23 +11,6 @@ const getTimeSeriesByRange = (coin = 'BTC', dateStart, dateEnd, granularity, cur
     },
     headers: {
       'User-Agent': 'Cryptonium',
-=======
-=======
->>>>>>> (feat) Get time series by range
-const getTimeSeriesByRange = (coin = 'BTC',  dateStart = Date.now() - 86400000, dateEnd = Date.now(), granularity = 600, currency = 'USD') => {
-  const options = {
-    url: `https://api.gdax.com/products/${coin}-${currency}/candles`,
-    qs: {
-      start: new Date(dateStart),
-      end: new Date(dateEnd),
-      granularity: granularity,
-    },
-    headers: {
-      'User-Agent': 'Request-Promise',
-<<<<<<< HEAD
->>>>>>> (feat) Get time series by range
-=======
->>>>>>> (feat) Get time series by range
     },
     json: true,
   };
@@ -39,44 +20,19 @@ const getTimeSeriesByRange = (coin = 'BTC',  dateStart = Date.now() - 86400000, 
     .catch(err => console.error(err));
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> (feat) Implement ticker route to fetch spot, bid, ask prices
 const getTickerData = (coin = 'BTC', currency = 'USD') => {
   const options = {
     url: `https://api.gdax.com/products/${coin}-${currency}/ticker`,
     headers: {
-<<<<<<< HEAD
       'User-Agent': 'Cryptonium',
-=======
-      'User-Agent': 'Request-Promise',
->>>>>>> (feat) Implement ticker route to fetch spot, bid, ask prices
     },
     json: true,
   };
 
   return rp(options)
-<<<<<<< HEAD
     .then(data => data)
     .catch(err => console.error(err));
 };
 
 module.exports.getTimeSeriesByRange = getTimeSeriesByRange;
 module.exports.getTickerData = getTickerData;
-=======
-module.exports.getTimeSeriesByRange = getTimeSeriesByRange;
->>>>>>> (feat) Get time series by range
-=======
-module.exports.getTimeSeriesByRange = getTimeSeriesByRange;
->>>>>>> (feat) Get time series by range
-=======
-    .then(series => series)
-    .catch(err => console.error(err));
-};
-
-module.exports.getTimeSeriesByRange = getTimeSeriesByRange;
-module.exports.getTickerData = getTickerData;
->>>>>>> (feat) Implement ticker route to fetch spot, bid, ask prices
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const router = require('./routes');
 const path = require('path');
 const compression = require('compression');
+>>>>>>> (feat) Get time series by range
 
 const app = express();
 app.use(morgan('dev'));
@@ -11,6 +12,14 @@ app.use(express.static(path.join(__dirname, '/../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/range', (req, res) => {
+  let coin = req.body.coin;
+  let dateStart = req.body.dateStart;
+  let dateEnd = req.body.dateEnd;
+  gdax.getTimeSeriesByRange(coin, dateStart, dateEnd)
+    .then(series => res.json(series));
+  res.sendStatus(200);
+});
 
 app.use(compression());
 app.use('/', router);
