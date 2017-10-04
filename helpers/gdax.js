d678f23d6fa45d28017a599819184ca25227d4c5
dulynoded <administrator@administrators-Mac-Pro.local>
2017-10-03 19:05:16 -0700

    (feat) Get time series by range

---
+++ helpers/gdax.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const rp = require('request-promise');
+
+const getTimeSeriesByRange = (coin = 'BTC',  dateStart = Date.now() - 86400000, dateEnd = Date.now(), granularity = 600, currency = 'USD') => {
+  const options = {
+    url: `https://api.gdax.com/products/${coin}-${currency}/candles`,
+    qs: {
+      start: new Date(dateStart),
+      end: new Date(dateEnd),
+      granularity: granularity,
+    },
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+    json: true,
+  };
+
+  return rp(options)
+    .then(series => series)
+    .catch(err => console.error(err));
+};
+
+module.exports.getTimeSeriesByRange = getTimeSeriesByRange;
