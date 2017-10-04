d01be29e367d43df795ef96bc6e6e4272f999a16
Claire Johnson <c.eliseee@gmail.com>
2017-10-03 17:20:33 -0700

    (test) Test for modal appearance

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+=======
+//TODO:
+// import react components to test
+// Do something to component (call functions, set state, etc)
+// Run assertion tests.
+>>>>>>> (test) Test for modal appearance
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -14,6 +21,24 @@ describe('<App/>', () => {
   it('should have a modal on popup', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Modal')).to.have.length(1);
+<<<<<<< HEAD
+=======
+  });
+});
+
+describe('<Search/>', () => {
+  it('should have a toggle and add button', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper.find('button')).to.have.length(2);
+  });
+
+  it('should intialize to all false coin selections', () => {
+    const wrapper = shallow(<Search />);
+    const state = wrapper.state();
+    expect(state.isBitcoin).to.equal(false);
+    expect(state.isLitecoin).to.equal(false);
+    expect(state.isEtherium).to.equal(false);
+>>>>>>> (test) Test for modal appearance
   });
 });
 
