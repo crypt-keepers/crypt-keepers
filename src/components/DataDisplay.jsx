64e32593909e46fce75f914ee6a3e35280456e3d
Claire Johnson <c.eliseee@gmail.com>
2017-10-04 12:45:49 -0700

    (feat) Add styling to data display when data changes

--- src/components/DataDisplay.jsx
+++ src/components/DataDisplay.jsx
@@ -4,11 +4,67 @@ import PropTypes from 'prop-types';
 import helpers from '../helpers/api-helpers';
 
 const propTypes = {
-  coin: PropTypes.string,
+  activeCoin: PropTypes.string,
 };
 
 const defaultProps = {
-  coin: '',
+  activeCoin: '',
+};
+
+const renderTimeSeriesData = (coinData) => {
+  // clear svg before every render
+  d3.selectAll('svg').remove();
+
+  // svg / line graph settings, hardcoded, customizable
+  const width = 600;
+  const height = 400;
+  const padding = 40;
+  const xTicks = 6;
+  const yTicks = 4;
+
+  // extract time and close info from data
+  const data = [];
+  coinData.map(row => data.push({ time: new Date(row[0] * 1000), close: row[4] }));
+
+  const svg = d3.select('#data-display')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  // x range is set this way because data given reverse-chronologically
+  const x = d3.scaleTime()
+    .range([padding, (width - padding)]);
+
+  const y = d3.scaleLinear()
+    .range([(height - padding), padding]);
+
+  // set x and y axis, use extend to calculate domain
+  const xAxis = d3
+    .axisBottom(x.domain(d3.extent(data, d => d.time)))
+    .ticks(xTicks);
+  const yAxis = d3
+    .axisLeft(y.domain(d3.extent(data, d => d.close)))
+    .ticks(yTicks);
+
+  // generate line
+  const line = d3.line()
+    .x(d => x(d.time))
+    .y(d => y(d.close));
+
+  // append everything
+  svg.append('g')
+    .attr('transform', `translate(0, ${height - padding})`)
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', `translate(${padding}, 0)`)
+    .call(yAxis);
+
+  svg.append('path')
+    .data([data])
+    .attr('fill', 'none')
+    .attr('stroke', 'blue')
+    .attr('d', line);
 };
 
 class DataDisplay extends React.Component {
@@ -17,93 +73,44 @@ class DataDisplay extends React.Component {
     this.state = {
       coinData: [],
       range: '1D',
+      isLoading: false,
     };
     this.getRangeData = this.getRangeData.bind(this);
-    this.renderTimeSeriesData = this.renderTimeSeriesData.bind(this);
   }
 
   componentDidMount() {
-    const coin = (this.props.coin.length) ? this.props.coin.split(' ')[0] : 'bitcoin';
+    const coin = (this.props.activeCoin.length) ? this.props.activeCoin : 'bitcoin';
     this.getRangeData(coin, '1D');
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.coin !== nextProps.coin) {
-      const coin = (nextProps.coin.length) ? nextProps.coin.split(' ')[0] : 'bitcoin';
+    if (this.props.activeCoin !== nextProps.activeCoin) {
+      const coin = (nextProps.activeCoin.length) ? nextProps.activeCoin : 'bitcoin';
       this.getRangeData(coin, '1D');
     }
   }
 
   getRangeData(coin, range) {
+    this.spinner(true);
     helpers.getRangeData(coin, range)
       .then((coinData) => {
-        this.setState({ coinData, range }, () => this.renderTimeSeriesData(this.state.coinData));
+        this.setState({ coinData, range }, () => renderTimeSeriesData(this.state.coinData));
+        this.spinner(false);
       });
   }
 
-  renderTimeSeriesData(coinData) {
-    // clear svg before every render
-    d3.selectAll('svg').remove();
-
-    // svg / line graph settings, hardcoded, customizable
-    const width = 600;
-    const height = 400;
-    const padding = 40;
-    const xTicks = 6;
-    const yTicks = 4;
-
-    // extract time and close info from data
-    const data = [];
-    coinData.map(row => data.push({ time: new Date(row[0] * 1000), close: row[4] }));
-
-    const svg = d3.select('#data-display')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
-
-    // x range is set this way because data given reverse-chronologically
-    const x = d3.scaleTime()
-      .range([padding, (width - padding)]);
-
-    const y = d3.scaleLinear()
-      .range([(height - padding), padding]);
-
-    // set x and y axis, use extend to calculate domain
-    const xAxis = d3
-      .axisBottom(x.domain(d3.extent(data, d => d.time)))
-      .ticks(xTicks);
-    const yAxis = d3
-      .axisLeft(y.domain(d3.extent(data, d => d.close)))
-      .ticks(yTicks);
-
-    // generate line
-    const line = d3.line()
-      .x(d => x(d.time))
-      .y(d => y(d.close));
-
-    // append everything
-    svg.append('g')
-      .attr('transform', `translate(0, ${height - padding})`)
-      .call(xAxis);
-
-    svg.append('g')
-      .attr('transform', `translate(${padding}, 0)`)
-      .call(yAxis);
-
-    svg.append('path')
-      .data([data])
-      .attr('fill', 'none')
-      .attr('stroke', 'blue')
-      .attr('d', line);
+  spinner(isSpinning) {
+    this.setState({ isLoading: isSpinning });
   }
 
-
   render() {
-    const coinName = (this.props.coin !== '') ? this.props.coin.split(' ')[0] : 'bitcoin';
+    const coinName = (this.props.activeCoin.length) ? this.props.activeCoin : 'bitcoin';
 
     return (
-      <div>DataDisplay for {coinName}
-        <div id="data-display"></div>
+      <div className="data-display-container">DataDisplay for {coinName}
+        <div id="data-display" />
+        <div className={this.state.isLoading ? 'data-overlay' : ''} />
+        <div className={this.state.isLoading ? 'loader' : ''} />
         <button onClick={() => this.getRangeData(coinName, '1D')}>1D</button>
         <button onClick={() => this.getRangeData(coinName, '1W')}>1W</button>
         <button onClick={() => this.getRangeData(coinName, '1M')}>1M</button>
