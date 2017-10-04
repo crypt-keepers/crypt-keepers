c5c5e5aa695132d89fc44f2c8204c6a3a70173b3
Nandakishore Saboo <nsaboo@nsaboo-mn.lan>
2017-10-03 21:41:39 -0700

    adds server side tests

---
+++ server/test/test.js
@@ -0,0 +1,44 @@
+// const app = require('../index.js');
+// const request = require('request');
+// const expect = require('chai').expect;
+
+// const port = 4568;
+
+
+describe('Cryptonoium Server tests', () => {
+  before(() => {
+    // runs before all tests in this block
+  });
+
+  after(() => {
+    // runs after all tests in this block
+  });
+
+  beforeEach(() => {
+    // runs before each test in this block
+  });
+
+  afterEach(() => {
+    // runs after each test in this block
+  });
+
+  describe('Cryptonoium User tests', () => {
+    it('Should get user details');
+    it('Should post user details');
+  });
+
+  describe('Cryptonoium Search tests', () => {
+    it('Should get search details');
+    it('Should post search details');
+  });
+
+  describe('Cryptonoium Range tests', () => {
+    it('Should get range details');
+    it('Should post range details');
+  });
+
+  describe('Cryptonoium List tests', () => {
+    it('Should get list details');
+    it('Should post list details');
+  });
+});
