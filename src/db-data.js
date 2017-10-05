68dad745a18ecacfae14c3fa7faec415875f4465
Clara Cho <clara.cho@gmail.com>
2017-10-04 21:29:38 -0700

    implement my finance, api-helpers

---
+++ src/db-data.js
@@ -0,0 +1,6 @@
+const Nanda = { username: 'Nanda', BTC: 5, ETH: 0, LTC: 1 };
+const Claire = { username: 'Claire', BTC: 1, ETH: 2, LTC: 10 };
+const Cecily = { username: 'Cecily', BTC: 1, ETH: 5, LTC: 1 };
+const Clara = { username: 'Clara', BTC: 2, ETH: 0, LTC: 1 };
+
+module.exports = { Nanda, Claire, Cecily, Clara };
