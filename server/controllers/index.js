f293ef5297660d7377177629b26a5a36af22418e
dulynoded <administrator@administrators-Mac-Pro.local>
2017-10-06 23:52:43 -0700

    (feat) Add MACD

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -14,7 +14,9 @@ module.exports = {
         .then((series) => {
           // console.log(ta.sma(series));
           // console.log(ta.ema(series));
-          console.log(ta.bollinger(series));
+          // console.log(ta.bollinger(series));
+          // console.log(ta.macd(series));
+
           res.json(series);
         })
         .catch((err) => {
