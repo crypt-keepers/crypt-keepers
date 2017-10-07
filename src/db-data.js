a72f3c7d53e96d3f192f752fb0253a9d6a10bd81
Clara Cho <clara.cho@gmail.com>
2017-10-06 18:45:18 -0700

    minor pie chart animation, assign color to coin

--- src/db-data.js
+++
@@ -1,6 +0,0 @@
-const Nanda = { username: 'Nanda', position: { BTC: 5, ETH: 0, LTC: 1 } };
-const Claire = { username: 'Claire', position: { BTC: 1, ETH: 2, LTC: 10 } };
-const Cecily = { username: 'Cecily', position: { BTC: 1, ETH: 5, LTC: 1 } };
-const Clara = { username: 'Clara', position: { BTC: 2, ETH: 0, LTC: 1 } };
-
-module.exports = { Nanda, Claire, Cecily, Clara };
