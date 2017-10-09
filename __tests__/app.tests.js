0737a05dc6ad7f36824c0eb66510353dbeac9357
Claire Johnson <c.eliseee@gmail.com>
2017-10-09 12:10:51 -0700

    (cleanup) remove unused imports in tests

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -1,31 +1,15 @@
-import React from 'react';
-import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import nock from 'nock';
-import App from '../src/components/App';
-import News from '../src/components/News';
-import NewsItem from '../src/components/NewsItem';
-import Overview from '../src/components/Overview';
-import DataDisplay from '../src/components/DataDisplay';
 import * as appActions from '../src/actions/appActions';
-import * as myFinanceActions from '../src/actions/myFinanceActions';
 import * as newsActions from '../src/actions/newsActions';
-import * as overviewActions from '../src/actions/overviewActions';
 import changePanelSelection from '../src/actions/panelActions';
 import * as reducers from '../src/reducers/reducer';
 
-
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
-
-configure({ adapter: new Adapter() });
-
 describe('App actions', () => {
   it('should create an action to change username', () => {
-    const username = 'Inigo Montoya'
+    const username = 'Inigo Montoya';
     const expectedAction = {
       type: 'CHANGE_USERNAME',
       payload: {
@@ -36,7 +20,7 @@ describe('App actions', () => {
   });
 
   it('should create an action to change coin', () => {
-    const coin = 'Bitcoin'
+    const coin = 'Bitcoin';
     const expectedAction = {
       type: 'CHANGE_COIN',
       payload: {
@@ -49,7 +33,7 @@ describe('App actions', () => {
 
 describe('News actions', () => {
   it('should create an action to change selected news', () => {
-    const selection = 'Bitcoin'
+    const selection = 'Bitcoin';
     const expectedAction = {
       type: 'CHANGE_NEWS_SELECTION',
       payload: { selection },
@@ -60,7 +44,7 @@ describe('News actions', () => {
 
 describe('Panel actions', () => {
   it('should create an action to change selected panel', () => {
-    const selection = 'overview'
+    const selection = 'overview';
     const expectedAction = {
       type: 'CHANGE_PANEL_SELECTION',
       payload: { selection },
@@ -147,92 +131,3 @@ describe('Reducer', () => {
     })).to.deep.equal({ username: 'bob', position: {} });
   });
 });
-
-
-
-// describe('todos reducer', () => {
-//
-//   it('should handle ADD_TODO', () => {
-//     expect(
-//       reducer([], {
-//         type: types.ADD_TODO,
-//         text: 'Run the tests'
-//       })
-//     ).toEqual([
-//       {
-//         text: 'Run the tests',
-//         completed: false,
-//         id: 0
-//       }
-//     ])
-//
-//     expect(
-//       reducer(
-//         [
-//           {
-//             text: 'Use Redux',
-//             completed: false,
-//             id: 0
-//           }
-//         ],
-//         {
-//           type: types.ADD_TODO,
-//           text: 'Run the tests'
-//         }
-//       )
-//     ).toEqual([
-//       {
-//         text: 'Run the tests',
-//         completed: false,
-//         id: 1
-//       },
-//       {
-//         text: 'Use Redux',
-//         completed: false,
-//         id: 0
-//       }
-//     ])
-//   })
-// })
-
-
-
-
-// describe('<App/>', () => {
-//   it('should have a modal on popup', () => {
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.find('Modal')).to.have.length(1);
-//   });
-// });
-//
-// describe('<News/>', () => {
-//   it('should have a News buttons', () => {
-//     const wrapper = shallow(<News />);
-//     expect(wrapper.find('button')).to.have.length(2);
-//   });
-//
-//   it('should render the right number of trending NewsItems', () => {
-//     const wrapper = shallow(<News />);
-//     const state = wrapper.state();
-//     const data = state.trending;
-//     expect(wrapper.find('NewsItem')).to.have.length(data.length);
-//   });
-// });
-//
-// describe('<NewsItem/>', () => {
-//   it('should have a link to article', () => {
-//     const wrapper = shallow(<NewsItem />);
-//     expect(wrapper.find('a')).to.have.length(1);
-//   });
-// });
-//
-// describe('<Overview/>', () => {
-//   it('should render a table', () => {
-//     const wrapper = shallow(<Overview />);
-//     expect(wrapper.find('table')).to.have.length(1);
-//   });
-//   it('should render data from all three coins', () => {
-//     const wrapper = shallow(<Overview />);
-//     expect(wrapper.find('TableRow')).to.have.length(3);
-//   });
-// });
